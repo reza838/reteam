ffd7b160f0f2f41bc87713c34358a3979925e2aa
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-06-21 11:47:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -128,11 +128,11 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://uploader.caliph.my.id/file/kgnuvhvd86.jpg'
+global.thumbdoc = 'https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg'
 
 /*=========== FAKE SIZE ===========*/
-global.fsizedoc = '99999999999999' // default 10TB
-global.fpagedoc = '999'
+global.fsizedoc = '99999999999999999999999999999999' // default 10TB
+global.fpagedoc = '99999999999999999999999999999999'
 
 /*=========== HIASAN ===========*/
 // DEFAULT MENU
